2662ed5eb41ef0404d4bbce904718c84611233b1
Odon-dave <141236618+Odon-dave@users.noreply.github.com>
2025-01-06 18:48:17 +0300

    database initilise

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ import sqlite3
 
 app = Flask(__name__)
 DATABASE = "shopping_list.db"
+
+
+
+def init_db():
+    """Initialize the SQLite database."""
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS shopping_list (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                item_name TEXT NOT NULL,
+                quantity INTEGER NOT NULL DEFAULT 1
+            )
+        """)
+        conn.commit()
